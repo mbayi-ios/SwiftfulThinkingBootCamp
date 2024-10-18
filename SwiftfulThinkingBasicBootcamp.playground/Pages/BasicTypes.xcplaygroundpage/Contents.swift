b99e93a5d15b9237ae0d4647065ba176acfd13cd
Ambrose <ambymbayi45@gmail.com>
2024-10-18 18:59:18 +0300

    learn basic types

--- SwiftfulThinkingBasicBootcamp.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
+++ SwiftfulThinkingBasicBootcamp.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// String - is regular text
+let myFirstItem: String = "Hello, world!"
+let meFirstItem = "Hello, world!"
+
+
+// Reference previously created objects
+let myTitle = myFirstItem
+
+
+// Boolean - Bool is true or false
+let mySecondItem: Bool = true
+let myThirditem = false
+
+// Swift is a type-safe language
+//let myFirthItem: Bool = "hello world"
+// let mySixthItem: String = false
+let mySeventItem = true
+
+
+// Date
+let myFirstDate: Date = Date()
+
+// Number can be Int, Double, CGFloat & More
+// Int is a whole Integer
+let myFirstNumber: Int = 1
+
+// user Double for math
+let mySecondNumber: Double = 1.0
+
+// use CG
+let myThirdNumber: CGFloat = 1.0
--- SwiftfulThinkingBasicBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
+++ SwiftfulThinkingBasicBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -1,3 +1,18 @@
-import Foundation
+import UIKit
 
 var greeting = "Hello, playground"
+print(greeting)
+
+// this is a single line comment
+// There is a comment
+
+/*
+ This is a multi-line comments
+ that goes to multiple lines!
+ */
+
+
+// naming conversions
+
+let firstGreeting = "hello world!"
+
